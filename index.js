7dac6f313014aca63ef3b17a3f7564fc52fde257
Thenlie <leithen113@gmail.com>
2023-04-01 13:10:20 -0600

    v1 complete

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ dotenv.config();
 const START_TIME = moment().subtract(1, 'hour').format('YYYYMMDDHHmm');
 const END_TIME = moment().format('YYYYMMDDHHmm');
 
+const GOOD_DIRECTIONS = ['S', 'SSW', 'SSE'];
+
 const URL = `https://api.synopticdata.com/v2/stations/timeseries?&token=${process.env.API_TOKEN}&stid=FPS,UUNET&status=active&output=json&start=${START_TIME}&end=${END_TIME}&units=temp|f,speed|mph`
 
 const response = await fetch(URL);
@@ -20,7 +22,6 @@ const body = await response.json();
  * wind_speed_set_1, wind_cardinal_direction_set_1d, precip_accum_five_minute_set_1
  */
 const observations = body.STATION[0].OBSERVATIONS;
-console.log(observations);
 
 /**
  * Given some weather data, return whether or not those conditions are flyable.
@@ -34,10 +35,23 @@ const isFlyable = (precip, wind_speed, wind_dir) => {
     // Check if any rain has fallen
     const isRaining = precip.find(i => i != 0) === undefined ? false : true;
     if (isRaining) return false;
+    console.log('Rain check good ✅')
 
     // Check wind direction, good values are S, SSW, SSE
+    let good_dirs = 0;
+    for (let i = 0; i < wind_dir.length; i++) {
+        if (GOOD_DIRECTIONS.includes(wind_dir[i])) good_dirs++;
+    }
+    if (good_dirs / wind_dir.length < 0.6) return false;
+    console.log('Wind direction check good ✅')
 
     // Check wind speeds, good values are 5-15
+    let good_speeds = 0;
+    for (let i = 0; i < wind_speed.length; i++) {
+        if (wind_speed[i] >= 5 && wind_speed[i] <= 15) good_speeds++;
+    }
+    if (good_speeds / wind_speed.length < 0.8) return false;
+    console.log('Wind speed check good ✅')
 
     return true;
 }
